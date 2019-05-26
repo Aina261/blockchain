d763c2f325bd089cdfb3cacf6f1016a8583a15ce
Aina <a.ramiarasoa@gmail.com>
2019-05-26 08:47:57 +0200

    add block class

---
+++ src/block/block.ts
@@ -0,0 +1,13 @@
+export default class Block implements Block {
+    constructor (
+        public index: number = 0,
+        public hash: string = '',
+        public previousHash: string = '',
+        public nonce: number = 0,
+        public transaction: any[] = []
+    ) {}
+
+    get key(): string {
+        return JSON.stringify(this.transaction) + this.index + this.previousHash + this.nonce;
+    }
+}
